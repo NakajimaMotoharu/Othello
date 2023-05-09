a68621d19ac8ababc74c155c09135dc731526beb
Nakajima Motoharu <132326083+NakajimaMotoharu@users.noreply.github.com>
2023-05-09 11:45:48 +0900

    Update Ai.java

--- src/Ai.java
+++ src/Ai.java
@@ -1,15 +1,21 @@
 public class Ai {
-	// Ai on off
+	// AIの使用判別フラグ
 	public static boolean use = false;
-	// Ai turn
+	// AIのターン判別フラグ
 	public static boolean turn = false;
+	// AIの配置位置保存場所
+	public static int x = 0, y = 0;
 
+	// AIの動作関数
 	public static void autoPush(int c){
+		// 左上から置ける場所を探してあったら置く(なかったらスキップ)
 		for (int i = 0; i < Othello.MAP_SIZE; i = i + 1){
 			for (int j = 0; j < Othello.MAP_SIZE; j = j + 1){
 				if (Othello.fPush(i, j, c)){
 					Othello.push(i, j, c);
 					turn = false;
+					x = i;
+					y = j;
 					return;
 				}
 			}
