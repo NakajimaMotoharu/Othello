6f69c545fb930c0adec24471614ac6679921ca0a
Nakajima Motoharu <132326083+NakajimaMotoharu@users.noreply.github.com>
2023-05-09 12:18:15 +0900

    Update IoCtrl.java

--- src/IoCtrl.java
+++ src/IoCtrl.java
@@ -1,4 +1,4 @@
 public class IoCtrl {
-	// Key frag
+	// キーのプッシュフラグなど
 	public static boolean w = false, a = false, s = false, d = false, c = false, f = false, r = false, pw = true, pa = true, ps = true, pd = true, pc = true, pf = true, pr = true;
 }
