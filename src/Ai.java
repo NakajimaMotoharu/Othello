5b7ccdd6eb2661e11fe5de4fc5893e3bb4826c32
中嶋 元春 <132326083+NakajimaMotoharu@users.noreply.github.com>
2023-05-22 21:37:07 +0900

    Update Ai.java

--- src/Ai.java
+++ src/Ai.java
@@ -8,29 +8,116 @@ public class Ai {
 
 	// AIの動作関数
 	public static void autoPush(int c){
-		// 一番多くひっくりかえせる場所に配置
-		int max = 0, tmpX = 0, tmpY = 0;
-		boolean frag = false;
+		// 一番多くひっくりかえせる場所に配置(例外あり)
+		// 四隅におかれる可能性がある所には極力おかない
+		// 四隅はできるだけ置く
+		int max = 0, tmpX = 0, tmpY = 0, gX = 0, gY = 0;
+		boolean frag = false, ng = false, good = false;
 
 		for (int i = 0; i < Othello.MAP_SIZE; i = i + 1){
 			for (int j = 0; j < Othello.MAP_SIZE; j = j + 1){
 				if (Othello.fPush(i, j, c)){
 					frag = true;
-					if (max <= Othello.calcPush(i, j, c)){
-						max = Othello.calcPush(i, j, c);
-						tmpX = i;
-						tmpY = j;
+					if (!(searchNgPoint(i, j))){
+						ng = true;
+						if (max <= Othello.calcPush(i, j, c)) {
+							max = Othello.calcPush(i, j, c);
+							tmpX = i;
+							tmpY = j;
+						}
+					}
+					if (searchOkPoint(i, j)){
+						good = true;
+						gX = i;
+						gY = j;
+					}
+				}
+			}
+		}
+
+		if ((frag) && !(ng)){
+			for (int i = 0; i < Othello.MAP_SIZE; i = i + 1) {
+				for (int j = 0; j < Othello.MAP_SIZE; j = j + 1) {
+					if (Othello.fPush(i, j, c)) {
+						if (max <= Othello.calcPush(i, j, c)) {
+							max = Othello.calcPush(i, j, c);
+							tmpX = i;
+							tmpY = j;
+						}
 					}
 				}
 			}
 		}
 
 		if (frag){
-			Othello.push(tmpX, tmpY, c);
-			x = tmpX;
-			y = tmpY;
+			if (good){
+				Othello.push(gX, gY, c);
+				x = gX;
+				y = gY;
+			} else {
+				Othello.push(tmpX, tmpY, c);
+				x = tmpX;
+				y = tmpY;
+			}
 		}
 
 		turn = false;
+	}
+
+	// 四隅近い場所かどうか
+	private static boolean searchNgPoint(int x, int y){
+		if ((x == 1) && (y == 0)){
+			return true;
+		}
+		if ((x == 1) && (y == 1)){
+			return true;
+		}
+		if ((x == 0) && (y == 1)){
+			return true;
+		}
+		if ((x == Othello.MAP_SIZE - 1) && (y == 1)){
+			return true;
+		}
+		if ((x == Othello.MAP_SIZE - 2) && (y == 1)){
+			return true;
+		}
+		if ((x == Othello.MAP_SIZE - 2) && (y == 0)){
+			return true;
+		}
+		if ((x == 0) && (y == Othello.MAP_SIZE - 2)){
+			return true;
+		}
+		if ((x == 1) && (y == Othello.MAP_SIZE - 2)){
+			return true;
+		}
+		if ((x == 1) && (y == Othello.MAP_SIZE - 1)){
+			return true;
+		}
+		if ((x == Othello.MAP_SIZE - 2) && (y == Othello.MAP_SIZE - 1)){
+			return true;
+		}
+		if ((x == Othello.MAP_SIZE - 2) && (y == Othello.MAP_SIZE - 2)){
+			return true;
+		}
+		if ((x == Othello.MAP_SIZE - 1) && (y == Othello.MAP_SIZE - 2)){
+			return true;
+		}
+		return false;
+	}
+
+	private static boolean searchOkPoint(int x, int y){
+		if ((x == 0) && (y == 0)){
+			return true;
+		}
+		if ((x == Othello.MAP_SIZE - 1) && (y == 0)){
+			return true;
+		}
+		if ((x == 0) && (y == Othello.MAP_SIZE - 1)){
+			return true;
+		}
+		if ((x == Othello.MAP_SIZE - 1) && (y == Othello.MAP_SIZE - 1)){
+			return true;
+		}
+		return false;
 	}
 }
